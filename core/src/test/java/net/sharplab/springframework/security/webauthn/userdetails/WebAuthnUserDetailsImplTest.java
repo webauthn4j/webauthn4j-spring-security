d7e838fcdf95dd1d7f73e2ba51d49d718417e7f5
ynojima <mail@ynojima.net>
2018-07-01 16:46:07 +0900

    Add some test cases

---
+++ core/src/test/java/net/sharplab/springframework/security/webauthn/userdetails/WebAuthnUserDetailsImplTest.java
@@ -0,0 +1,31 @@
+package net.sharplab.springframework.security.webauthn.userdetails;
+
+
+import com.webauthn4j.authenticator.Authenticator;
+import com.webauthn4j.authenticator.AuthenticatorImpl;
+import org.junit.Test;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.Collections;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class WebAuthnUserDetailsImplTest {
+
+    @Test
+    public void getter_setter_test(){
+        GrantedAuthority grantedAuthority = new SimpleGrantedAuthority("ROLE_ADMIN");
+        Authenticator authenticator = new AuthenticatorImpl(null, null, 0);
+        WebAuthnUserDetailsImpl userDetails = new WebAuthnUserDetailsImpl(
+                "dummy",
+                "dummy",
+                Collections.singletonList(authenticator),
+                Collections.singletonList(grantedAuthority));
+
+        userDetails.setPasswordAuthenticationAllowed(true);
+        assertThat(userDetails.isPasswordAuthenticationAllowed()).isTrue();
+        assertThat(userDetails.getAuthenticators()).isEqualTo(Collections.singletonList(authenticator));
+    }
+
+}
