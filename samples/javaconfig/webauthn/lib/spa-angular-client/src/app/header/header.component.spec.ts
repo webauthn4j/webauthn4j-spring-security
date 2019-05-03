2b2d6bfed2446353850c752aa23465ca9f2100d3
ynojima <mail@ynojima.net>
2019-05-03 23:51:18 +0900

    Enable test case again

--- samples/javaconfig/webauthn/lib/spa-angular-client/src/app/header/header.component.spec.ts
+++ samples/javaconfig/webauthn/lib/spa-angular-client/src/app/header/header.component.spec.ts
@@ -1,60 +1,61 @@
-// /*
-//  * Copyright 2002-2019 the original author or authors.
-//  *
-//  * Licensed under the Apache License, Version 2.0 (the "License");
-//  * you may not use this file except in compliance with the License.
-//  * You may obtain a copy of the License at
-//  *
-//  *     http://www.apache.org/licenses/LICENSE-2.0
-//  *
-//  * Unless required by applicable law or agreed to in writing, software
-//  * distributed under the License is distributed on an "AS IS" BASIS,
-//  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-//  * See the License for the specific language governing permissions and
-//  * limitations under the License.
-//  */
-//
-// import {async, ComponentFixture, TestBed} from '@angular/core/testing';
-//
-// import {HeaderComponent} from './header.component';
-// import {AuthService} from "../auth/auth.service";
-// import {of} from "rxjs/internal/observable/of";
-// import {RouterTestingModule} from "@angular/router/testing";
-//
-// describe('HeaderComponent', () => {
-//   let component: HeaderComponent;
-//   let fixture: ComponentFixture<HeaderComponent>;
-//
-//   beforeEach(async(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [ HeaderComponent ],
-//       imports: [
-//         RouterTestingModule
-//       ]
-//     }).overrideComponent(HeaderComponent, {
-//       set:{
-//         providers: [
-//           {
-//             provide: AuthService,
-//             useFactory: ()=>{
-//               let authServiceMock = new AuthService(null, null);
-//               spyOn(authServiceMock, "getAuthenticationStatus").and.returnValue(of("Anonymous"));
-//               return authServiceMock;
-//             }
-//           }
-//         ]
-//       }
-//     })
-//     .compileComponents();
-//   }));
-//
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(HeaderComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
-//
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+/*
+ * Copyright 2002-2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+
+import {HeaderComponent} from './header.component';
+import {AuthService} from "../auth/auth.service";
+import {of} from "rxjs/internal/observable/of";
+import {RouterTestingModule} from "@angular/router/testing";
+import {scheduled} from "rxjs";
+
+describe('HeaderComponent', () => {
+  let component: HeaderComponent;
+  let fixture: ComponentFixture<HeaderComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ HeaderComponent ],
+      imports: [
+        RouterTestingModule
+      ]
+    }).overrideComponent(HeaderComponent, {
+      set:{
+        providers: [
+          {
+            provide: AuthService,
+            useFactory: ()=>{
+              let authServiceMock = new AuthService(null, null);
+              spyOn(authServiceMock, "getAuthenticationStatus").and.returnValue(of("Anonymous"));
+              return authServiceMock;
+            }
+          }
+        ]
+      }
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(HeaderComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
